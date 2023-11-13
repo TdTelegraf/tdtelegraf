8bc6e467fd4586ecb28c73df43506d671d0a1ed4
Igor Suvorov <hi@isuvorov.com>
2023-11-13 18:17:50 +0300

    fix: types may be

--- libs/telegraf/src/LskTelegram.ts
+++ libs/telegraf/src/LskTelegram.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { getEnvConfig, Logger } from '@lskjs/log';
-import * as http from 'http';
 import { Telegram } from 'telegraf';
-import { Opts } from 'telegraf/types';
+// import { Opts } from 'telegraf/types';
+import { Opts, Telegram as TelegramType } from 'typegram';
 
 const log = new Logger({
   ns: 'telegram',
@@ -10,47 +10,24 @@ const log = new Logger({
   level: 'error',
 });
 
-namespace ApiClient {
-  export type Agent = http.Agent | ((parsedUrl: URL) => http.Agent) | undefined;
-  export interface Options {
-    /**
-     * Agent for communicating with the bot API.
-     */
-    agent?: http.Agent;
-    /**
-     * Agent for attaching files via URL.
-     * 1. Not all agents support both `http:` and `https:`.
-     * 2. When passing a function, create the agents once, outside of the function.
-     *    Creating new agent every request probably breaks `keepAlive`.
-     */
-    attachmentAgent?: Agent;
-    apiRoot: string;
-    /**
-     * @default 'bot'
-     * @see https://github.com/tdlight-team/tdlight-telegram-bot-api#user-mode
-     */
-    apiMode: 'bot' | 'user';
-    webhookReply: boolean;
-    testEnv: boolean;
-  }
-
-  export interface CallApiOptions {
-    signal?: AbortSignal;
-  }
+export interface CallApiOptions {
+  signal?: AbortSignal;
 }
 
 export class LskTelegram extends Telegram {
-  async callApi<M extends keyof Telegram>(
+  // @ts-ignore
+  async callApi<M extends keyof TelegramType>(
     method: M,
     payload: Opts<M>,
-    { signal }: ApiClient.CallApiOptions = {},
-  ): Promise<ReturnType<Telegram[M]>> {
+    { signal }: CallApiOptions = {},
+  ): Promise<ReturnType<TelegramType[M]>> {
     log.trace('LskTelegram.callApi start', { method, payload, signal });
+    // @ts-ignore
     const res = await super.callApi<M>(method, payload, { signal });
     //
     log.trace('LskTelegram.callApi finish', { method, payload, signal });
 
-    return res;
+    return res as ReturnType<TelegramType[M]>;
   }
 }
 
